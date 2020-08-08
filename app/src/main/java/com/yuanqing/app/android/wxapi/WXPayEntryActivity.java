f0f27b7b9f9ee25faff7489fcda9021be2562448
zhengzhuang95 <zheng951203@163.com>
2020-08-08 09:02:31 +0800

    first commit

---
+++ app/src/main/java/com/yuanqing/app/android/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,12 @@
+
+
+package com.yuanqing.app.android.wxapi;
+
+import io.dcloud.feature.payment.weixin.AbsWXPayCallbackActivity;
+
+public class WXPayEntryActivity extends AbsWXPayCallbackActivity{
+	
+}
+
+
+
